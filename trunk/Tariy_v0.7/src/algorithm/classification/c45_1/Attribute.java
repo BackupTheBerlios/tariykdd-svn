c2e603d3695b493cda3c7391479001a9a1f0e867
andress <andress@11e62675-681f-0410-a5cd-b6d5982e6390>
2006-12-29 18:39:04 +0000

    Node in TreeCounter and TreeFinal for C4.5

---
+++ trunk/Tariy_v0.7/src/algorithm/classification/c45_1/Attribute.java
@@ -0,0 +1,130 @@
+/*
+ * Attribute.java
+ *
+ * Created on 22 de junio de 2006, 11:13
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package algorithm.classification.c45_1;
+
+/**
+ *
+ * @author and
+ */
+public class Attribute {
+    String name;
+    double entropia;
+    int frecuence;
+    int frecuenceFather;
+    Attribute son;
+    Attribute brother;
+    
+    /** Creates a new instance of Attribute */
+    public Attribute(String name, Attribute son, Attribute brother) {
+        this.name = name;
+        frecuence = 1;
+        entropia = 0.0;
+        this.son = son;
+        this.brother = brother;
+    }
+    
+    public int getFrecuence() {
+        return frecuence;
+    }
+    
+    public void incrementFrecuence(){
+        frecuence++;
+    }
+    
+    
+    public double setEntropia(){
+        double probabilidadInterna = 0.0;
+        float division;
+        float divExt;
+        Attribute auxSon = this.son;
+        while(auxSon != null){
+            division = ((float)auxSon.frecuence / (float)this.frecuence);
+            probabilidadInterna += (division) * log2(division);
+            auxSon = auxSon.brother;
+        }
+        this.entropia = probabilidadInterna;
+        return probabilidadInterna;
+    }
+    
+    public double log2(double value){ // lo cambie de private a public
+        if(value == 0.0) return 0.0;
+        return Math.log(value)/TreeCounter.LOG2;
+    }
+    
+    public Attribute bestChild(String target){
+        Attribute major = this.son;
+        Attribute next = this.son.brother;
+        
+        while(next != null){
+            if(major.frecuence < next.frecuence){
+                major = next;
+            }
+            next = next.brother;
+        }
+        major.name = target + "=" + major.name;
+        major.frecuenceFather = this.frecuence;
+        major.brother = null;
+        return major;
+    }
+    
+    public Attribute getChild(int index) {
+        Attribute attribute = this.son;
+        int count = 0;
+        
+        while(count < index) {
+            attribute = attribute.brother;
+            count++;
+        }
+        return attribute;
+    }
+    
+    //este devuelve cuantos hijos tiene este nodo
+    public int getChildCount() {
+        Attribute attribute = this.son;
+        int children = 1;
+        
+        while(attribute.brother != null) {
+            attribute = attribute.brother;
+            children++;
+        }
+        return children;
+    }
+    
+    //este devuelve dependiendo el nodo que le pasemos cual es su indice como hijo de este nodo
+    public int getIndexOfChild(Object child) {
+        Attribute attribute = this.son;
+        int index = 0;
+        
+        while(attribute.brother != null) {
+            attribute = attribute.brother;
+            if(attribute.equals(child))
+                break;
+            else
+                index++;
+        }
+        return index;
+    }
+    
+    //devuelve si este nodo es una hoja o no (se complementa en C45TreeModel)
+    public Attribute isLeaf() {
+        Attribute attribute = this;
+        return attribute.son;
+    }
+    
+    public String toString(){
+//        return new String("[" + this.name + ", " + this.frecuence + ", " +
+//                this.entropia + "]");
+        if(this.isLeaf() == null){
+            return name + " [" + this.frecuence + "/" + this.frecuenceFather + "]";
+        } else {
+            return name;
+        }
+    }
+}
