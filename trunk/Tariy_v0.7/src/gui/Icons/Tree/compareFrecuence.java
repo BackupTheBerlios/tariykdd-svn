3cf0e10816c7a73d99c489a9fef8777005362706
andress <andress@11e62675-681f-0410-a5cd-b6d5982e6390>
2007-01-15 23:14:00 +0000

    Compare by Frecuence a classifiaction rule

---
+++ trunk/Tariy_v0.7/src/gui/Icons/Tree/compareFrecuence.java
@@ -0,0 +1,28 @@
+/*
+ * compareFrecuence.java
+ *
+ * Created on 15 de enero de 2007, 17:00
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package gui.Icons.Tree;
+import algorithm.classification.c45_1.Attribute;
+import algorithm.classification.c45_1.Leaf;
+import java.util.*;
+
+/**
+ *
+ * @author and
+ */
+public class compareFrecuence implements Comparator {
+    
+    public int compare(Object obj1, Object obj2) {
+        int s1 = ((Attribute)((Leaf)obj1).getLeaf()).getFrecuence();
+        int s2 = ((Attribute)((Leaf)obj2).getLeaf()).getFrecuence();
+        
+        return s2 - s1;
+    }
+}
+
