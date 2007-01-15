d7407418df85ce168480ac9c6865f49e8530eb68
andress <andress@11e62675-681f-0410-a5cd-b6d5982e6390>
2007-01-15 23:34:51 +0000

    Support to TreeTableModel

--- trunk/Tariy_v0.7/src/algorithm/classification/c45_1/Leaf.java
+++ trunk/Tariy_v0.7/src/algorithm/classification/c45_1/Leaf.java
@@ -18,32 +18,54 @@ import java.util.LinkedList;
  * @author and
  */
 public class Leaf {
-    String path;
-    Attribute leaf;
+    private String path;
+    private Attribute leaf;
+    private int sizeRule;
     /** Creates a new instance of Leaf */
     public Leaf(Attribute leaf, LinkedList path) {
         this.leaf = leaf;
+        this.sizeRule = path.size();
         this.path = this.buildStringPath(path);
     }
-    
+
     private String buildStringPath(LinkedList path){
         StringBuffer str = new StringBuffer();
         int size = path.size();
         
         for(int i = 0; i < size; i++){
-            //if(i % 2 == 0){
-            //    str.append(path.get(i) + "=");
-            //}else{
-                if(i != size - 1){
-                    str.append(path.get(i) + " and ");
-                } else {
-                    str.append(path.get(i));
-                }
-            //}
+            if(i != size - 1){
+                str.append(path.get(i) + " and ");
+            } else {
+                str.append(path.get(i));
+            }
         }
         return str.toString();
     }
-    
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setLeaf(Attribute leaf) {
+        this.leaf = leaf;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Attribute getLeaf() {
+        return leaf;
+    }
+
+    public void setSizeRule(int sizeRule) {
+        this.sizeRule = sizeRule;
+    }
+
+    public int getSizeRule() {
+        return sizeRule;
+    }
+
     public String toString() {
         return path + " --> " + leaf;
     }
