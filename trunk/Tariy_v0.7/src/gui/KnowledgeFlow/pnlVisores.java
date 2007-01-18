1c163502af8e525ba988ce79951adc3a09d7805c
andress <andress@11e62675-681f-0410-a5cd-b6d5982e6390>
2007-01-18 20:30:53 +0000

    new  icons

--- trunk/Tariy_v0.7/src/gui/KnowledgeFlow/pnlVisores.java
+++ trunk/Tariy_v0.7/src/gui/KnowledgeFlow/pnlVisores.java
@@ -28,6 +28,7 @@ public class pnlVisores extends javax.swing.JPanel {
         jLabel12 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setName("otro");
@@ -53,11 +54,18 @@ public class pnlVisores extends javax.swing.JPanel {
         jLabel13.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
 
         jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Visores/filefind.png")));
-        jLabel14.setText("Weka_tree");
+        jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Visores/WekaTree.png")));
+        jLabel14.setText("Weka_Tree");
         jLabel14.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jLabel14.setName("Weka_tree");
+        jLabel14.setName("Weka_Tree");
         jLabel14.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+
+        jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Visores/texTree.png")));
+        jLabel15.setText("Text_Tree");
+        jLabel15.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel15.setName("Weka_Tree");
+        jLabel15.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
@@ -69,6 +77,7 @@ public class pnlVisores extends javax.swing.JPanel {
                     .add(jLabel12, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 122, Short.MAX_VALUE)
                     .add(jLabel11, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 122, Short.MAX_VALUE)
                     .add(org.jdesktop.layout.GroupLayout.TRAILING, jLabel14, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 122, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jLabel15, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 122, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                     .add(jLabel13, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 122, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
@@ -81,9 +90,11 @@ public class pnlVisores extends javax.swing.JPanel {
                 .add(jLabel11)
                 .add(16, 16, 16)
                 .add(jLabel14)
-                .add(15, 15, 15)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jLabel15)
+                .add(14, 14, 14)
                 .add(jLabel13)
-                .addContainerGap(69, Short.MAX_VALUE))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
     
@@ -93,6 +104,7 @@ public class pnlVisores extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
     // End of variables declaration//GEN-END:variables
     
 }
