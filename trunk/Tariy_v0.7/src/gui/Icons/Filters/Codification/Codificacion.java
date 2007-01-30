d222f25609d4501e040a784ba286f23b54a90a4e
andress <andress@11e62675-681f-0410-a5cd-b6d5982e6390>
2007-01-30 22:48:29 +0000

    whit coments

--- trunk/Tariy_v0.7/src/gui/Icons/Filters/Codification/Codificacion.java
+++ trunk/Tariy_v0.7/src/gui/Icons/Filters/Codification/Codificacion.java
@@ -26,17 +26,31 @@ import java.util.Collections;
 import javax.swing.table.AbstractTableModel;
 
 /**
+ * This class is used for create a new Table whit codified data.<br>  
+ * This in particular contains information about AbstractTableModel 
+ * whit output data.
  *
  * @author Tariy
  */
 public class Codificacion extends AbstractTableModel{
-    
+    /** The input data that arrive from a connection. */
     AbstractTableModel datosEntrada;
+    
+    /** The Data dictionary. */
     public ValAtributosCod valatricod;
     
+    /** Data after to have applied the filter. */
     final Object[][] datos;
+    
+    /** Names of the attributes. */
     final String[] nomcol;
     
+   /** 
+   * This initializes and resizes the variables
+   * and constructs the new table.
+   *
+   * @param dataIn input data that arrive from a connection.
+   */
     public Codificacion(AbstractTableModel dataIn){
         datosEntrada = dataIn;
         valatricod = new ValAtributosCod(dataIn);
@@ -45,6 +59,10 @@ public class Codificacion extends AbstractTableModel{
         nuevaTabla();
     }
     
+   /**
+   * This function constructs the new table  
+   * whit filtered data
+   */
     public void nuevaTabla() {
         for(int f = 0; f < datosEntrada.getRowCount(); f++) {
             for(int c = 0; c < datosEntrada.getColumnCount(); c++) {
@@ -59,26 +77,58 @@ public class Codificacion extends AbstractTableModel{
         }
     }
     
+    /**
+     *  Returns the number of columns of the table. 
+     */
     public int getColumnCount() {
         return nomcol.length;
     }
     
+    /**
+     *  Returns the number of rows of the table 
+     */
     public int getRowCount() {
         return datosEntrada.getRowCount();
     }
-    
+       
+    /**
+     *  Returns a default name for the column 
+     *
+     * @param column  the column being queried
+     * @return a string containing the default name of <code>column</code>
+     */
     public String getColumnName(int col) {
         return nomcol[col];
     }
     
+     /**
+     *  Returns the value of a cell queried, in a row and column of the table.
+     *
+     *  @param  row  the row being queried
+     *  @param  col the column being queried
+     *  @return datos value of a cell queried
+     */
     public Object getValueAt(int row, int col) {
         return datos[row][col];
     }
     
+     /**
+     *  Returns <code>Object.class</code> regardless of <code>columnIndex</code>.
+     *
+     *  @param c  the column being queried
+     *  @return the Object.class
+     */
     public Class getColumnClass(int c) {
         return getValueAt(0, c).getClass();
     }
     
+     /**
+     *  Returns if the cell is editable.  This is the default implementation for all cells.
+     *
+     *  @param  row  the row being queried
+     *  @param  col the column being queried
+     *  @return boolean value that depends if it is editable
+     */
     public boolean isCellEditable(int row, int col) {
         //Note that the data/cell address is constant,
         //no matter where the cell appears onscreen.
@@ -89,6 +139,13 @@ public class Codificacion extends AbstractTableModel{
         }
     }
     
+     /**
+     *  this method assigns a value to a cell.
+     *
+     *  @param  value   value to assign to cell
+     *  @param  row   row of cell
+     *  @param  col  column of cell
+     */
     public void setValueAt(Object value, int row, int col) {
         datos[row][col] = value;
     }
--- trunk/Tariy_v0.7/src/gui/Icons/Filters/Codification/ValAtributosCod.java
+++ trunk/Tariy_v0.7/src/gui/Icons/Filters/Codification/ValAtributosCod.java
@@ -1,11 +1,4 @@
 /*
- * FiltroStandard.java
- *
- * Created on 18 de mayo de 2006, 03:18 PM
- *
- */
-
-/*
  *    This program is free software; you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation; either version 2 of the License, or
@@ -29,23 +22,39 @@ import java.util.StringTokenizer;
 import javax.swing.table.AbstractTableModel;
 
 /**
+ * This class is used for create a Data dictionary.<br>  
+ * This in particular contains information about AbstractTableModel 
+ * whit Data dictionary.
  *
  * @author Tariy
  */
 public class ValAtributosCod extends AbstractTableModel{
-    
+    /** The input data that arrive of the class Codification */
     AbstractTableModel datosEntrada;
     
-    public final ArrayList datos = new ArrayList();   //  el 30 hay que hacerlo dinamico ***---989898
+    /** Data whit values of dictionary. */
+    public final ArrayList datos = new ArrayList();   
+    
+    /** Names of the attributes. */
     final String[] nomcol = new String[3];
     
     int catri = 1, limc = 0,  auxc = 0;
     
+   /** 
+   * This initializes and resizes the variables
+   * and constructs the new table.
+   *
+   * @param dataIn input data that arrive from a connection whit Codification.
+   */
     public ValAtributosCod(AbstractTableModel dataIn){
         datosEntrada = dataIn;
         nuevaTabla();
     }
     
+   /**
+   * This function constructs the new table  
+   * whit data dictionary.
+   */
     public void nuevaTabla() {
         Object[] valores = new Object[datosEntrada.getRowCount()];
         int con = 0, pv;
@@ -83,18 +92,37 @@ public class ValAtributosCod extends AbstractTableModel{
         Collections.sort(datos);
     }
     
+   /**
+    *  Returns the number of columns of the table. 
+    */
     public int getColumnCount() {
         return nomcol.length;
     }
     
+   /**
+    *  Returns the number of rows of the table 
+    */
     public int getRowCount() {
         return auxc;
     }
-    
+     
+    /**
+     *  Returns a default name for the column 
+     *
+     * @param column  the column being queried
+     * @return a string containing the default name of <code>column</code>
+     */  
     public String getColumnName(int col) {
         return nomcol[col];
     }
-    
+  
+    /**
+     *  Returns the value of a cell queried, in a row and column of the table.
+     *
+     *  @param  row  the row being queried
+     *  @param  col the column being queried
+     *  @return datos value of a cell queried
+     */  
     public Object getValueAt(int row, int col) {
         StringTokenizer token = new StringTokenizer((String)datos.get(row), "=");
         if(col == 0){
@@ -105,13 +133,15 @@ public class ValAtributosCod extends AbstractTableModel{
             token.nextToken();
             return token.nextToken();
         }
-//        
-//        
-//        ArrayList fila = (ArrayList)datos.get(row);
-//        return fila.get(col);
         return null;
     }
     
+     /**
+     *  Returns <code>Object.class</code> regardless of <code>columnIndex</code>.
+     *
+     *  @param c  the column being queried
+     *  @return the Object.class
+     */
     public Class getColumnClass(int c) {
         if(c == 0){
             return Short.class;
@@ -120,6 +150,13 @@ public class ValAtributosCod extends AbstractTableModel{
         }
     }
     
+     /**
+     *  Returns if the cell is editable.  This is the default implementation for all cells.
+     *
+     *  @param  row  the row being queried
+     *  @param  col the column being queried
+     *  @return boolean value that depends if it is editable
+     */
     public boolean isCellEditable(int row, int col) {
         //Note that the data/cell address is constant,
         //no matter where the cell appears onscreen.
@@ -128,10 +165,5 @@ public class ValAtributosCod extends AbstractTableModel{
         } else {
             return true;
         }
-    }
-    
-//    public void setValueAt(Object value, int row, int col) {
-//        datos[row][col] = value;
-//    }
-    
+    }  
 }
